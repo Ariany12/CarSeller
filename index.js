90d96b4c398ef3670b69a1dc3cce19693768fb98
Ariany Mendes <ariany_mendes@yahoo.com>
2023-03-09 23:20:33 -0500

    Replace the nav bar with flexbox , add my api in the index.js , added some design in signup page , master some css in the stely.css , added new picutes in the website

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+let btn = document.querySelector('#btn');
+let div = document.querySelector('#joke');
+btn.addEventListener('click', myFunc);async function myFunc() { 
+     let category = 'animal'; 
+let response = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`); 
+let parsedRes = await response.json();  console.log(parsedRes);  div.innerHTML = parsedRes.value;
+}
