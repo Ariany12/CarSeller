94e8ec7893f5e2105e5edc4ec4f2a45555f69a09
Ariany Mendes <ariany_mendes@yahoo.com>
2023-03-10 14:30:35 -0500

    finaling css

---
+++ post.js
@@ -0,0 +1,17 @@
+// async function fetchPosts(){
+    
+//     try{
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//     if(!response.ok){
+//         throw new Error(`Failed to fetch posts: ${response.status}`);
+//     }
+
+//     return response.json();
+// }   catch(e) {
+//     console.log(e);
+//     }
+// }
+
+
+// Function listsPosts()
